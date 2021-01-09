89eabc8f2399da14e96470ed3947d2131c43731e
muycul <48526137+muycul@users.noreply.github.com>
2021-01-09 17:06:49 -0500

    Update cul-automatewoo-functions.php

--- cul-automatewoo-functions/cul-automatewoo-functions.php
+++ cul-automatewoo-functions/cul-automatewoo-functions.php
@@ -6,7 +6,7 @@
  * @package cul-automatewoo-functions
  *
  * Plugin Name:       CUL - AutomateWoo Custom Functions
- * Description:       Plugin that adds CUL´s Custom functions to AutomateWoo
+ * Description:       Plugin that adds CUL's Custom functions to AutomateWoo
  * Version:           1.0
  * Author:            CUL
  */
